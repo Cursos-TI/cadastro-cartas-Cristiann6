3bdb94b91e6922249895bf516e2a0456f32053a7
Cristiann6 <cristian.freitas444@gmail.com>
2025-09-13 23:53:24 +0000

    inserindo a carta 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,19 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-
+//variaveis da carta 1
   char Estado[20], Codigo[20], Cidade[20];
   int Populacao;
   float Area;
   float PIB;
   int Turistico;
+
+  //variaveis da carta 2
+  char Estado2[20], Codigo2[20], Cidade2[20];
+  int Populacao2;
+  float Area2;
+  float PIB2;
+  int Turistico2;
 
 
 printf("Carta 1: \n");
@@ -36,11 +43,40 @@ scanf("%f", &PIB);
 printf("Dígite o seu ponto turístico: \n");
 scanf("%d", &Turistico);
 
-  // Área para exibição dos dados da cidade
+printf("Carta 2: \n");
+
+printf("Dígite o seu estado: \n");
+scanf("%s" , Estado2);
+
+printf("Dígite o seu código: \n");
+scanf("%s" , Codigo2);
+
+printf("Dígite a sua cidade: \n");
+scanf("%s" , Cidade2);
+
+printf("Dígite a sua população: \n");
+scanf("%d" , &Populacao2);
+
+printf("Dígite a sua área: \n");
+scanf("%f", &Area2);
+
+printf("Dígite o seu pib: \n");
+scanf("%f", &PIB2);
+
+printf("Dígite o seu ponto turístico: \n");
+scanf("%d", &Turistico2);
+
+  // Área para exibição da carta 1
 printf("estado: %s - código: %s\n", Estado, Codigo);
 printf("cidade: %s - população: %d\n", Cidade, Populacao);
 printf("área: %f - pib: %f\n", Area, PIB);
 printf("número de pontos turisticos: %d", Turistico);
+
+// Área para exibição da carta 2
+printf("estado: %s - código: %s\n", Estado2, Codigo2);
+printf("cidade: %s - população: %d\n", Cidade2, Populacao2);
+printf("área: %f - pib: %f\n", Area2, PIB2);
+printf("número de pontos turisticos: %d", Turistico2);
 
 return 0;
 } 
